aa8f916803cadad74e015eb9b6943dbe9482bd34
kalpasuraweera <kalpamadhushan18@gmail.com>
2024-06-30 14:04:28 +0530

    q2

--- q1.scala
+++ q1.scala
@@ -1,7 +1,7 @@
 @main def main(): Unit =print(reverseString("UCSC"))
 
 
-def reverseString(s:String): String = 
+def reverseString(s: String): String = 
     val length: Int = s.length()
     length match{
         case 0 => s
--- q2.scala
+++ q2.scala
@@ -1,5 +1,29 @@
-@main def main(): Unit = print(filteredStrings())
+import scala.io.StdIn.readLine
+import scala.io.StdIn.readInt
 
-def filteredStrings(): Array[String] =
-    val inputs = readLine()
-    Array("kalpa")
+@main def main(): Unit = print(filteredStrings().mkString(", "))
+
+def filteredStrings(): Array[String] = filterStrings(readStrings(readNumberOfStrings()))
+
+def readStrings(numStrings: Int): Array[String] =
+    numStrings match {
+        case x if x > 1 => Array(readString()).concat(readStrings(numStrings - 1))
+        case _ => Array(readString())
+    }
+
+def readString(): String = 
+    print("Enter String: ")
+    readLine()
+
+def readNumberOfStrings(): Int =
+    print("Enter Number of Strings: ")
+    readInt()
+
+
+def filterStrings(allStrings: Array[String]): Array[String] =
+    val allStringsLength: Int = allStrings.length
+    allStringsLength match{
+        case x if x > 1  => filterStrings(Array(allStrings(0))).concat(filterStrings(allStrings.slice(1, allStringsLength)))
+        case x if allStrings(0).length() > 5 => allStrings
+        case _ => Array()
+    }
